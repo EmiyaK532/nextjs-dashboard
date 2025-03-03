c8aeb257c926d654b492cc361d7f9d1f1dbfe842
AmizukiZ <3073877601@qq.com>
2025-03-03 19:39:21 +0800

    chapter-11-adding search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,35 @@
+//这是一个客户端组件
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+// import { useEffect } from 'react';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  // useEffect(()=>{
+  //   console.log(searchParams)
+  // }, [])
+
+  const handleSearch = (term: string) => {
+    // console.log(term)
+    // console.log(searchParams.get('query'))
+    const params = new URLSearchParams(searchParams)
+    // console.log(params)
+    if(term) {
+      params.set('query', term)
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+    console.log("Current searchParams: ", params.toString())
+    console.log(params.get('query'))
+  }
+  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +38,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e )=> {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental:{
+    //允许为特定的路由采用PRR（Partial PreRendering）
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
